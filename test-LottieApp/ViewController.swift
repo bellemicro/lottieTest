11f9c14515bdfa0b39dfee5cf529c070e1e0ca8c
Zhang_yan <bellemicro@outlook.com>
2019-06-18 15:57:59 +0800

    Initial Commit

---
+++ test-LottieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test-LottieApp
+//
+//  Created by mobstazinc_ios_developer on 2019/6/18.
+//  Copyright © 2019年 mobstazinc_ios_developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ test-LottieAppTests/test_LottieAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  test_LottieAppTests.swift
+//  test-LottieAppTests
+//
+//  Created by mobstazinc_ios_developer on 2019/6/18.
+//  Copyright © 2019年 mobstazinc_ios_developer. All rights reserved.
+//
+
+import XCTest
+@testable import test_LottieApp
+
+class test_LottieAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
