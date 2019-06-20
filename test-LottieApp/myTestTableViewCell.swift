c4463b481f044b957fa049f4c47f8e3aa9572cde
Zhang_yan <bellemicro@outlook.com>
2019-06-20 11:25:38 +0800

    first commit

---
+++ test-LottieApp/myTestTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  myTestTableViewCell.swift
+//  test-LottieApp
+//
+//  Created by mobstazinc_ios_developer on 2019/6/19.
+//  Copyright © 2019年 mobstazinc_ios_developer. All rights reserved.
+//
+
+import UIKit
+
+class myTestTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ test-LottieApp/switchBtnViewController.swift
@@ -0,0 +1,30 @@
+//
+//  switchBtnViewController.swift
+//  test-LottieApp
+//
+//  Created by mobstazinc_ios_developer on 2019/6/19.
+//  Copyright © 2019年 mobstazinc_ios_developer. All rights reserved.
+//
+
+import UIKit
+
+class switchBtnViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
